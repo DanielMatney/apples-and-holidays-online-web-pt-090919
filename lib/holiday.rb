041e4bf9ece3388579e015ee7e55d4fb1e600c5e
Daniel Matney <danieljmatney@gmail.com>
2019-09-24 06:53:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash[:winter].each do |holiday, supplies|
-    p holiday.to_s.capitalize!
+    p holiday.split.capitalize!.join
   end
 end
 
