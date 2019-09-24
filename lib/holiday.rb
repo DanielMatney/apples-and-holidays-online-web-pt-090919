e81f67d2f71c0a0926d45ffdbc63faaea27392e9
Daniel Matney <danieljmatney@gmail.com>
2019-09-24 07:12:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,6 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].each do |holiday, supplies|
     holiday.to_s.split("_").each do |word|
     word.capitalize!
-    puts holiday
     end
   end
 end
