6af78e352fa75307aaf23f072672819f03f503a4
Daniel Matney <danieljmatney@gmail.com>
2019-09-24 07:22:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_winter_holiday_supplies(holiday_hash)
       word.capitalize!
     end
     holiday = holiday.join(" ")
-    puts "#{holiday}: #{supplies.each}"
+    supplies.each {|supply| puts "#{holiday}: #{supply}"}
   end
 end
 
